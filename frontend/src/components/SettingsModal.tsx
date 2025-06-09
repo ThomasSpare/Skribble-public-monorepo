c6b8de31f19ecae2f117c3f2e7b0b7304d7902f1
ThomasSpare <t.spare.jkpg@gmail.com>
2025-06-10 01:51:28 +0200

    Fix SettingsModal

--- frontend/src/components/SettingsModal.tsx
+++ frontend/src/components/SettingsModal.tsx
@@ -754,8 +754,8 @@ export default function SettingsModal({ user, isOpen, onClose, onUserUpdate }: S
                         <label className="relative inline-flex items-center cursor-pointer ml-4">
                           <input 
                             type="checkbox" 
-                            checked={privacySettings[setting.key]}
-                            onChange={(e) => setPrivacySettings(prev => ({ ...prev, [setting.key]: e.target.checked }))}
+                            checked={typeof privacySettings[setting.key] === 'boolean' ? privacySettings[setting.key] as boolean : false}
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPrivacySettings((prev: PrivacySettings) => ({ ...prev, [setting.key]: e.target.checked }))}
                             className="sr-only peer" 
                           />
                           <div className="w-11 h-6 bg-skribble-plum peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-skribble-azure/20 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-skribble-azure"></div>
